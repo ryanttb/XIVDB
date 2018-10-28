9961bfd89da94636724c2616d33161205f0d7bdb
ryanttb <ryan@trippingthebits.com>
2018-10-28 13:58:53 -0400

    show first image

--- app/src/main/java/info/xivdb/xivdb/MainActivity.java
+++ app/src/main/java/info/xivdb/xivdb/MainActivity.java
@@ -3,9 +3,13 @@ package info.xivdb.xivdb;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.SearchView;
 import android.widget.TextView;
 
@@ -21,7 +25,42 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
+
+    public class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        private ImageView imageView;
+
+        public DownloadImageTask(ImageView imageView) {
+            this.imageView = imageView;
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            String url = urls[0];
+            Bitmap urlBitmap = null;
+
+            try {
+                InputStream inputStream = new URL(url).openStream();
+                urlBitmap = BitmapFactory.decodeStream(inputStream);
+            } catch (IOException ex) {
+                //log.e("Error", ex.getMessage());
+                ex.printStackTrace();
+            }
+            return urlBitmap;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+
+            imageView.setImageBitmap(bitmap);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,6 +105,17 @@ public class MainActivity extends AppCompatActivity {
 
                 TextView t = findViewById(R.id.textView);
                 t.setText(output);
+
+                if (results.length() > 0) {
+                    JSONObject firstResult = results.getJSONObject(0);
+                    ImageView firstCardImage = findViewById(R.id.firstCardImage);
+                    TextView firstCardText = findViewById(R.id.firstCardText);
+
+                    firstCardText.setText(firstResult.getString("name"));
+
+                    DownloadImageTask downloadImageTask = new DownloadImageTask(firstCardImage);
+                    downloadImageTask.execute(firstResult.getString("icon"));
+                }
             } catch (JSONException ex) {
                 // suppress
             }
